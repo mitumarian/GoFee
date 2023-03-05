9d593049fb8c9f793a0cf3fea607be5b248f5a29
mitumarian <mitumarian99@gmail.com>
2023-03-05 17:25:21 +0200

    Create basic folder structure

---
+++ GoFee/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+global using GoFee.Model;
+global using GoFee.ViewModel;
+global using System.Collections.ObjectModel;
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.Runtime.CompilerServices;
+global using System.Text.Json;
---
+++ GoFee/Model/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoFee.Model
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public int StoreID { get; set; }
+        public int ProductID { get; set; }
+        public int Stock { get; set; }
+    }
+}
