a1ad108750312fd4f0c3a7abfc3043836e3bf622
mitumarian <mitumarian99@gmail.com>
2023-03-05 16:31:03 +0200

    Initial version

---
+++ GoFee/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GoFee;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+        Routing.RegisterRoute("NewPage1", typeof(NewPage1));
+    }
+}
